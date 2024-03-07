b2fbd3a60264fa8ae5fc0b82149ed560c4a28f02
babukabarabuka <babukabarabuk@gmail.com>
2024-03-08 01:39:46 +0400

    Update ChatHandler.js

--- src/minecraft/handlers/ChatHandler.js
+++ src/minecraft/handlers/ChatHandler.js
@@ -120,9 +120,12 @@ class StateHandler extends EventHandler {
 				this.awaitingPartyVictim = false
 			}
 			if (this.isOfflinePlayerMessage(message)) {
-				this.bot.chat('/lobby')
-				//this.bot.chat(`/gc Couldn't warp out ${this.targetName}, they're offline. ` + this.getRandomFruit())
-				setTimeout((param1, param2) => {param1.chat(param2)}, 800, this.bot, `/gc Couldn't warp out ${this.targetName}, they're offline. ` + this.getRandomFruit())
+				//this.bot.chat('/lobby')
+				//setTimeout((param1, param2) => {param1.chat(param2)}, 800, this.bot, `/gc Couldn't warp out ${this.targetName}, they're offline. ` + this.getRandomFruit())
+				
+				this.bot.chat(`/gc Couldn't warp out ${this.targetName}, they're offline. ` + this.getRandomFruit())
+				setTimeout((param1) => {param1.chat('/lobby')}, 200, this.bot)
+
 				this.awaitingPartyVictim = false
 			}
 
