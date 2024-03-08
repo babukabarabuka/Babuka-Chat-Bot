b50872cbca03516f661c3d5a4eb33961abc8b9a1
babukabarabuka <babukabarabuk@gmail.com>
2024-03-08 16:06:57 +0400

    Update BallCommand.js

--- src/minecraft/commands/BallCommand.js
+++ src/minecraft/commands/BallCommand.js
@@ -7,7 +7,7 @@ class BallCommand extends MinecraftCommand {
 		this.name = '8ball'
 		this.aliases = ['ball']
 		this.description = 'Tells you the truth'
-		this.ballAnswers = 'Hell no!;My sources say no.;Dumb question, but no.;Don\'t count on it.;My reply is no.;Very doubtful.;Outlook not so good.;;;;;;;;;IDK.;Probably.;Dumb question.;Reply hazy, try again.;Cannot predict now.;Ask again later.;;;;;;;;;;;Dumb question, but yes.;Most Likely.;Yes, Definitely.;Without a doubt.;As I see it, yes.;Signs point to yes.; You may rely on it.; It is decidedly so.;Yes.;'.split(';')
+		this.ballAnswers = 'Hell no!;My sources say no.;Dumb question, but no.;Don\'t count on it.;My reply is no.;Very doubtful.;Outlook not so good.$$$$$$$$$$$$$;IDK.;Probably.;Dumb question.;Reply hazy, try again.;Cannot predict now.;Ask again later.$$$$$$$$$$$$$;Dumb question, but yes.;Most Likely.;Yes, Definitely.;Without a doubt.;As I see it, yes.;Signs point to yes.; You may rely on it.; It is decidedly so.;Yes.;'.replace('$', '').split(';')
 	}
 
 	getRandomInt(max) {
@@ -20,7 +20,7 @@ class BallCommand extends MinecraftCommand {
 			this.send('/gc 8ball: What a stupid question!')
 		}
 		else if (username.toLowerCase().includes('babuka') && message.includes('%')) {
-			this.send('/gc 8ball: What a great question! I\'m not sure about the answer, but I do know that the person asking this quesion is very smart.')
+			this.send('/gc 8ball: What a great question! I\'m not sure about the answer, but I do know that the person asking this quesion is very smart. ' + this.ballAnswers.length)
 		}
 		else {
 			this.send(`/gc 8ball: ${this.ballAnswers[this.getRandomInt(this.ballAnswers.length)]}`)
